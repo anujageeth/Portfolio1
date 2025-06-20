26b6f7a5b0dc8de75596c8706940f8dcca3cd3ce
Anuja Geeth <www.anujageeth@gmail.com>
2025-06-20 14:13:40 +0530

    UI improvements

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Layout from './components/Layout';
 import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
@@ -8,6 +8,9 @@ import SkillsPage from './pages/SkillsPage';
 import ContactPage from './pages/ContactPage';
 import ResumePage from './pages/ResumePage';
 import CreativeWorksPage from './pages/CreativeWorksPage';
+import Photography from './components/Photography';
+import GraphicDesign from './components/GraphicDesign';
+import VideoEditing from './components/VideoEditing';
 
 function App() {
   return (
@@ -20,7 +23,29 @@ function App() {
           <Route path="/skills" element={<SkillsPage />} />
           <Route path="/cv" element={<ResumePage />} />
           <Route path="/contact" element={<ContactPage />} />
+          
+          {/* Creative Works Routes */}
           <Route path="/creative" element={<CreativeWorksPage />} />
+          <Route path="/creative/photography" element={
+            <div className="standalone-creative-component">
+              <Photography />
+            </div>
+          } />
+          <Route path="/creative/graphic" element={
+            <div className="standalone-creative-component">
+              <GraphicDesign />
+            </div>
+          } />
+          <Route path="/creative/video" element={
+            <div className="standalone-creative-component">
+              <VideoEditing />
+            </div>
+          } />
+          
+          {/* Redirect for old URLs */}
+          <Route path="/creative-photography" element={<Navigate to="/creative/photography" replace />} />
+          <Route path="/creative-graphic" element={<Navigate to="/creative/graphic" replace />} />
+          <Route path="/creative-video" element={<Navigate to="/creative/video" replace />} />
         </Routes>
       </Layout>
     </BrowserRouter>
